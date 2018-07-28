22d53ec67b24874d6480cf94bd8deb4abf3b588c
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-28 08:12:26 +0200

    read data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("hello world");
+        Path trainingsamplePath = Paths.get("trainingsample.csv");
+        Files.readAllLines(trainingsamplePath);
+        System.out.println("done");
     }
 }
