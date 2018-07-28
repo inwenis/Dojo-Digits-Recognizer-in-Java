06a511595ccf2c7ee66acab58022fc0b576f5188
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-28 08:20:34 +0200

    split into columns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
+//        System.out.println("hello world");
         Path trainingsamplePath = Paths.get("trainingsample.csv");
-        Files.readAllLines(trainingsamplePath);
+        List<String> rows = Files.readAllLines(trainingsamplePath);
+//        String[] splittedSampleRow = rows.get(123).split(",");
+//        for (String column : splittedSampleRow) { System.out.println(column); }
+        fruitsTest();
+        List<String[]> rowsSplittedInColumns = rows.stream()
+                .map(x -> x.split(","))
+                .collect(Collectors.toList());
         System.out.println("done");
+    }
+
+    private static void fruitsTest() {
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("orange");
+        fruits.add("strawberry");
+
+        List<Integer> lengths = fruits.stream()
+                .map(x -> x.length())
+                .collect(Collectors.toList());
+
+        for (Integer length : lengths) { System.out.println(length); }
+
+        fruits.stream()
+                .map(x -> x.toUpperCase())
+                .forEach(System.out::println);
     }
 }
