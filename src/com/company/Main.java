f9373cc60835c3f90825e698e1ecdc27ccf65b3d
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-07-28 08:31:39 +0200

    map into records

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
 //        String[] splittedSampleRow = lines.get(123).split(",");
 //        for (String column : splittedSampleRow) { System.out.println(column); }
         fruitsTest();
-        List<List<Integer>> rows = lines.stream()
+        List<Record> records = lines.stream()
                 .map(x -> x.split(","))
                 .collect(Collectors.toList())
                 .subList(1, lines.size()) // removes header row
@@ -26,6 +26,12 @@ public class Main {
                 .map(x -> Arrays.stream(x)
                         .map(y -> Integer.parseInt(y))
                         .collect(Collectors.toList()))
+                .map(x -> {
+                    Record record = new Record();
+                    record.Digit = x.get(0);
+                    record.Pixels = x.subList(1, x.size()).toArray(new Integer[0]);
+                    return record;
+                })
                 .collect(Collectors.toList());
 
         System.out.println("done");
@@ -47,5 +53,24 @@ public class Main {
         fruits.stream()
                 .map(x -> x.toUpperCase())
                 .forEach(System.out::println);
+
+        fruits.stream()
+                .map(x -> {
+                    FruitInfo fruitInfo = new FruitInfo();
+                    fruitInfo.Name = x;
+                    fruitInfo.DoILikeThisFruit = true;
+                    return fruitInfo;
+                })
+                .forEach(x -> System.out.println(x.Name + " eat it: " + x.DoILikeThisFruit));
+    }
+
+    static class FruitInfo {
+        public String Name;
+        public boolean DoILikeThisFruit;
+    }
+
+    static class Record {
+        public Integer Digit;
+        public Integer[] Pixels;
     }
 }
